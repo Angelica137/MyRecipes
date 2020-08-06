f462d6bc927cb572390a546ca9ff76e38c143695
Angelica <Angelica137@users.noreply.github.com>
2020-08-06 18:22:06 +0100

    landing view

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,23 @@
 from app import app
+from flask import render_template
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello world!'
+    user = {'user_name': 'Angelica'}
+    recipes = [
+			{
+          'recipe': 'Fish Tacos',
+          'description': 'Delicious fish tacos from Tijuana',
+          'time': 25,
+          'calories': 400,
+			},
+			{			
+          'recipe': 'Chicken Katsu Curry',
+          'description': 'the best chicken katsu ever',
+          'time': 20,
+          'calories': 350,
+			}
+		]
+    return render_template('index.html', title='Home', user=user, recipes=recipes)
