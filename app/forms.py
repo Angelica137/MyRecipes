b242545d7c719e526367a828fe4e96dbe89b095b
Angelica <Angelica137@users.noreply.github.com>
2020-08-10 14:39:23 +0100

    add email validator

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField, SubmitField, PasswordField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, Length
 
 
 class SignUpForm(FlaskForm):
-    user_name = StringField('Name', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired()])
+    username = StringField('Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Sign up')
+
+    def validate_email(self, username):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('It seems you already have an account. Please log in.')
