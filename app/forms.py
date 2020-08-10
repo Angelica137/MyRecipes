2ad94af82f3fb9172916e6f24a0ad919d5a2b0e7
Angelica <Angelica137@users.noreply.github.com>
2020-08-10 15:13:00 +0100

    add user sing up mvc working

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField, SubmitField, PasswordField
 from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, Length
+from app.models import User
 
 
 class SignUpForm(FlaskForm):
@@ -9,7 +10,7 @@ class SignUpForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Sign up')
 
-    def validate_email(self, username):
+    def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError('It seems you already have an account. Please log in.')
