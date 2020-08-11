11e7f39d80c38e671fbb2308b5b504fb7b47b0d7
Angelica <Angelica137@users.noreply.github.com>
2020-08-11 14:26:22 +0100

    complete log in

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
 from app import app, db
 from flask import render_template, flash, redirect, url_for
-from app.forms import SignUpForm
+from app.forms import SignUpForm, LoginForm
 from app.models import User
+from flask_login import current_user, login_user, logout_user, login_required
 
 
 @app.route('/')
@@ -11,6 +12,7 @@ def index():
 
 
 @app.route('/home')
+@login_required
 def home():
     user = {'user_name': 'Angelica'}
     recipes = [
@@ -45,6 +47,16 @@ def sign_up():
     return render_template('sign_up.html', title='Join', form=form)
     
 
-#@app.route('/login', methods=['GET', 'POST'])
-# def login():
-    
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is  None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user)
+        return redirect(url_for('home'))
+    return render_template('login.html', title='Login', form=form)    
