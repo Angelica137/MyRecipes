eaca9caf74e3815b4aa8bd34d251f5a8af90ef27
Angelica <Angelica137@users.noreply.github.com>
2020-08-30 15:13:45 +0100

    add tag field to recipe form

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,8 @@
+import wtforms
 from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField, SubmitField, PasswordField, TextAreaField, IntegerField
 from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, Length
-from app.models import User, Recipe
+from app.models import User, Recipe, Tag, Ingredient
 
 
 class SignUpForm(FlaskForm):
@@ -22,11 +23,52 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Login')
 
 
+
+
+    
+class TagField(wtforms.StringField):
+    def _value(self):
+        if self.data:
+            '''
+            Display tags as a comma-separated list
+            '''
+            return ', '.join([tag.name for tag in self.data])
+        return ''
+
+    def get_tags_from_string(self, tag_string):
+        raw_tags = tag_string.split(',')
+        '''
+        Filter out any empty tags
+        '''
+        tag_names = [name.strip() for name in raw_tags if name.strip()]
+        '''
+        Query that database and retrieve any tags we have already save
+        '''
+        existing_tags = Tag.query.filter(Tag.name.in_(tag_names))
+        '''
+        Determine which tag names are new
+        '''
+        new_names = set(tag_names) - set([tag.name for tag in existing_tags])
+        '''
+        Create a  list of unsaved Tag instances for the new tags
+        '''
+        new_tags = [Tag(name=name) for name in new_names]
+        '''
+        Return all the existing tags + all the new, unsaved tags
+        '''
+        return list(existing_tags) + new_tags
+
+    def process_formdata(self, valuelist):
+        if valuelist:
+            self.data = self.get_tags_from_string(valuelist[0])
+        else:
+            self.data = []
+
+
 class RecipeForm(FlaskForm):
     name = StringField('Recipe name', validators=[DataRequired()])
     description = TextAreaField('Decription')
     cook_time = IntegerField('Cooking time')
     servings = IntegerField('Number of servings')
+    tags = TagField('Tags', description='Separate multiple tags with commas')
     submit = SubmitField('Add recipe')
-
-    
---
+++ migrations/versions/daa73497c362_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: daa73497c362
+Revises: e57587a151a6
+Create Date: 2020-08-30 15:02:26.279151
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'daa73497c362'
+down_revision = 'e57587a151a6'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
